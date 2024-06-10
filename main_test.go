4343a376e1e6e0f12819939889edc8074331968d
Boburmirzo <symonbobur@gmail.com>
2024-06-10 17:43:53 +0500

    Refactoring

--- main_test.go
+++ main_test.go
@@ -25,76 +25,76 @@ const (
 
 func TestGetNotExistingKey(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(get, url+"_temp", bytes.NewBufferString(""))
+	req, _ := http.NewRequest(get, url+"_temp", bytes.NewBufferString(""))
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
 	if err != nil || resp == nil || resp.Status != statusNotFound {
 		t.Error("TestGetNotExistingKey() failed", err, resp)
 	}
+	defer resp.Body.Close()
 }
 
 func TestStoreEmptyValue(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(post, url, bytes.NewBufferString(""))
+	req, _ := http.NewRequest(post, url, bytes.NewBufferString(""))
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
 	if err != nil || resp == nil || resp.Status != statusBadRequest {
 		t.Error("TestStoreEmptyValue() failed", err, resp)
 	}
+	defer resp.Body.Close()
 }
 
 func TestAppendEmptyValue(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
+	req, _ := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
 	resp, err := client.Do(req)
 	if err != nil || resp == nil || resp.Status != statusCreated {
 		t.Error("TestAppendEmptyValue() failed", err, resp)
 	}
 	resp.Body.Close()
 
-	req, err = http.NewRequest(put, url, bytes.NewBufferString(""))
+	req, _ = http.NewRequest(put, url, bytes.NewBufferString(""))
 	resp, err = client.Do(req)
-	defer resp.Body.Close()
 	if err != nil || resp == nil || resp.Status != statusBadRequest {
 		t.Error("TestAppendEmptyValue() failed", err, resp)
 	}
+	defer resp.Body.Close()
 }
 
 func TestAppendNotExistingKey(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(put, url+"_temp", bytes.NewBufferString(" world"))
+	req, _ := http.NewRequest(put, url+"_temp", bytes.NewBufferString(" world"))
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
 	if err != nil || resp == nil || resp.Status != statusNotFound {
 		t.Error("TestAppendNotExistingKey() failed", err, resp)
 	}
+	defer resp.Body.Close()
 }
 
 func TestDeleteNotExistingKey(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(delete, url+"_temp", bytes.NewBufferString(""))
+	req, _ := http.NewRequest(delete, url+"_temp", bytes.NewBufferString(""))
 	resp, err := client.Do(req)
-	defer resp.Body.Close()
 	if err != nil || resp == nil || resp.Status != statusNotFound {
 		t.Error("TestDeleteNotExistingKey() failed", err, resp)
 	}
+	defer resp.Body.Close()
 }
 
 func TestStoreAndGet(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
+	req, _ := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
 	resp, err := client.Do(req)
 	if err != nil || resp == nil || resp.Status != statusCreated {
 		t.Error("TestStoreAndGet() failed", err, resp)
 	}
 	resp.Body.Close()
 
-	req, err = http.NewRequest(get, url, bytes.NewBufferString(""))
+	req, _ = http.NewRequest(get, url, bytes.NewBufferString(""))
 	resp, err = client.Do(req)
-	defer resp.Body.Close()
 	if err != nil || resp == nil || resp.Status != statusOK {
 		t.Error("TestStoreAndGet() failed", err, resp)
 	}
+	defer resp.Body.Close()
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil || data == nil || string(data) != "hello" {
 		t.Error("TestStoreAndGet() failed", err, resp)
@@ -103,50 +103,50 @@ func TestStoreAndGet(t *testing.T) {
 
 func TestStoreAndDelete(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
+	req, _ := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
 	resp, err := client.Do(req)
 	if err != nil || resp == nil || resp.Status != statusCreated {
 		t.Error("TestStoreAndDelete() failed", err, resp)
 	}
 	resp.Body.Close()
 
-	req, err = http.NewRequest(delete, url, bytes.NewBufferString(""))
+	req, _ = http.NewRequest(delete, url, bytes.NewBufferString(""))
 	resp, err = client.Do(req)
 	if err != nil || resp == nil || resp.Status != statusNoContent {
 		t.Error("TestStoreAndDelete() failed", err, resp)
 	}
 	resp.Body.Close()
 
-	req, err = http.NewRequest(get, url, bytes.NewBufferString(""))
-	resp, err = client.Do(req)
-	defer resp.Body.Close()
+	req, _ = http.NewRequest(get, url, bytes.NewBufferString(""))
+	resp, _ = client.Do(req)
 	if err != nil || resp == nil || resp.Status != statusNotFound {
 		t.Error("TestStoreAndDelete() failed", err, resp)
 	}
+	defer resp.Body.Close()
 }
 
 func TestStoreAndAppend(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
+	req, _ := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
 	resp, err := client.Do(req)
 	if err != nil || resp == nil || resp.Status != statusCreated {
 		t.Error("TestStoreAndAppend() failed", err, resp)
 	}
 	resp.Body.Close()
 
-	req, err = http.NewRequest(put, url, bytes.NewBufferString(" world"))
+	req, _ = http.NewRequest(put, url, bytes.NewBufferString(" world"))
 	resp, err = client.Do(req)
 	if err != nil || resp == nil || resp.Status != statusOK {
 		t.Error("TestStoreAndAppend() failed", err, resp)
 	}
 	resp.Body.Close()
 
-	req, err = http.NewRequest(get, url, bytes.NewBufferString(""))
+	req, _ = http.NewRequest(get, url, bytes.NewBufferString(""))
 	resp, err = client.Do(req)
-	defer resp.Body.Close()
 	if err != nil || resp == nil || resp.Status != statusOK {
 		t.Error("TestStoreAndAppend() failed", err, resp)
 	}
+	defer resp.Body.Close()
 	data, err := ioutil.ReadAll(resp.Body)
 	if err != nil || data == nil || string(data) != "hello world" {
 		t.Error("TestStoreAndAppend() failed", err, resp)
@@ -155,7 +155,7 @@ func TestStoreAndAppend(t *testing.T) {
 
 func TestTTL(t *testing.T) {
 	client := &http.Client{}
-	req, err := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
+	req, _ := http.NewRequest(post, url+"?ttl=1", bytes.NewBufferString("hello"))
 	resp, err := client.Do(req)
 	if err != nil || resp == nil || resp.Status != statusCreated {
 		t.Error("TestTTL() failed", err, resp)
@@ -164,10 +164,10 @@ func TestTTL(t *testing.T) {
 
 	time.Sleep(2 * time.Second)
 
-	req, err = http.NewRequest(get, url, bytes.NewBufferString(""))
+	req, _ = http.NewRequest(get, url, bytes.NewBufferString(""))
 	resp, err = client.Do(req)
-	defer resp.Body.Close()
 	if err != nil || resp == nil || resp.Status != statusNotFound {
 		t.Error("TestTTL() failed", err, resp)
 	}
+	defer resp.Body.Close()
 }
